7f092facd7b051716f47470328fa531a01760fb4
FrallanBallan <143787464+FrallanBallan@users.noreply.github.com>
2024-06-06 15:00:58 +0200

    Built a navbar

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,5 +1,7 @@
 // import { Button, ButtonGroup } from "@chakra-ui/react";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import React from "react";
 
 function App() {
   return (
@@ -9,8 +11,8 @@ function App() {
         lg: `"nav nav" "aside main"`, //1024px
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="gold">
