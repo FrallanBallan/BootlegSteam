9d0542e7ecd16ddcd12d27cf8e7afafaa99eefa1
FrallanBallan <143787464+FrallanBallan@users.noreply.github.com>
2024-06-07 12:06:29 +0200

    Built the darkmode switch

---
+++ game-hub/src/components/ColorMode.tsx
@@ -0,0 +1,18 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import React from "react";
+
+const ColorMode = () => {
+  const { toggleColorMode, colorMode } = useColorMode(); //ColorMode defines current color theme
+  return (
+    <HStack>
+      <Switch
+        colorScheme="green"
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      ></Switch>
+      <Text>Dark Mode</Text>
+    </HStack>
+  );
+};
+
+export default ColorMode;
