b6021d84798bc6a494c9c330cc8279b0a4f2cb8d
FrallanBallan <143787464+FrallanBallan@users.noreply.github.com>
2024-06-11 11:13:16 +0200

    Build platform select

--- game-hub/src/components/NavBar.tsx
+++ game-hub/src/components/NavBar.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/Logo/logo.webp";
 import ColorMode from "./ColorMode";
+import PlatformSelector from "./PlatformSelector";
 
 const NavBar = () => {
   return (
     <HStack justifyContent={"space-between"} padding={"10px"}>
       <Image src={logo} boxSize="60px"></Image>
+
       <ColorMode />
     </HStack>
   );
