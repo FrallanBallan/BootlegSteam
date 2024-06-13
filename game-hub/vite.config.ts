8fa04017bc16b10dbf3f4e02374c6d254a9b5b6e
FrallanBallan <143787464+FrallanBallan@users.noreply.github.com>
2024-06-13 15:15:31 +0200

    more defines

--- game-hub/vite.config.ts
+++ game-hub/vite.config.ts
@@ -11,5 +11,8 @@ export default defineConfig({
     __DEFINES__: {}, // Replace with your actual definitions or default values
     __BASE__: '"/"', // or set to your base path if different
     __SERVER_HOST__: '""', // or set to your server host value if different
+    __HMR_PROTOCOL__: '""', // Hot Module Replacement protocol
+    __HMR_HOSTNAME__: '""', // Hot Module Replacement hostname
+    __HMR_PORT__: '""', // Hot Module Replacement port
   },
 })
